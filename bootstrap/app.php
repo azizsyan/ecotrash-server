6061878d2a42d220ad769b8eed2d5504ecc20352
azizsyan <azizsyanjaya7@gmail.com>
2026-05-31 13:01:41 +0700

    Refactor application bootstrap for improved readability and consistency in routing and middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,27 +7,26 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(
     basePath: dirname(__DIR__)
 )
-->withRouting(
-    web: __DIR__ . '/../routes/web.php',
-    api: __DIR__ . '/../routes/api.php',
-    commands: __DIR__ . '/../routes/console.php',
-    health: '/up',
-)
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
 
-->withMiddleware(function (
-    Middleware $middleware
-) {
+    ->withMiddleware(function (Middleware $middleware) {
 
-    $middleware->alias([
-        'superadmin' =>
-            \App\Http\Middleware\SuperAdminMiddleware::class,
-    ]);
+        $middleware->alias([
+            'superadmin' =>
+                \App\Http\Middleware\SuperAdminMiddleware::class,
 
-})
+            'role' =>
+                \App\Http\Middleware\RoleMiddleware::class,
+        ]);
 
-->withExceptions(function (
-    Exceptions $exceptions
-) {
+    })
 
-})
-->create();
+    ->withExceptions(function (Exceptions $exceptions) {
+
+    })
+    ->create();
